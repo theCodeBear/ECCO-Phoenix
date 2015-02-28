cd3d8bf2c620b12827b28aa1d0ae22b4124d9f4a
Todd Kronenberg <toddkronenberg@gmail.com>
2015-02-28 13:53:54 -0800

    Added the rest of the Angular Routes Added the rest of the the Angular routes, changed the name of the folder that holds the angular routes from 'partials' to 'templates'.

--- www/js/routes.js
+++ www/js/routes.js
@@ -5,10 +5,50 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
-  .state('intro', {
+  .state('home', {
     url: '/',
-    templateUrl: '../partials/intro.html',
+    templateUrl: '../templates/home.html',
     controller: 'home'
+  })
+  .state('event', {
+    url: '/event',
+    templateUrl: '../templates/event.html',
+    // controller: 'event'
+  })
+  .state('createEvent', {
+    url: '/createEvent',
+    templateUrl: '../templates/createEvent.html',
+    // controller: 'createEvent'
+  })
+  .state('matches', {
+    url: '/matches',
+    templateUrl: '../templates/matches.html',
+    // controller: 'matches'
+  })
+  .state('chat', {
+    url: '/chat',
+    templateUrl: '../templates/chat.html',
+    // controller: 'chat'
+  })
+  .state('matching', {
+    url: '/matching',
+    templateUrl: '../templates/matching.html',
+    // controller: 'matching'
+  })
+  .state('search', {
+    url: '/search',
+    templateUrl: '../templates/search.html',
+    // controller: 'search'
+  })
+  .state('editEvent', {
+    url: '/editEvent',
+    templateUrl: '../templates/editEvent.html',
+    // controller: 'editEvent'
+  })
+  .state('profile', {
+    url: '/profile',
+    templateUrl: '../templates/profile.html',
+    // controller: 'profile'
   });
 
 }]);
