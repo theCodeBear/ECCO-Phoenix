6f0c255f280d0bfb55c37e443d3a4d1f7af47d7a
Todd Kronenberg <toddkronenberg@gmail.com>
2015-03-01 14:59:30 -0800

    Fixed conflicts on front end

--- server/routes/event/show.js
+++ server/routes/event/show.js
@@ -6,8 +6,11 @@ module.exports = {
   handler: function(request,reply) {
     Event.findById(request.params.eventId).populate('attendees').exec(function(err, event) {
       console.log(event);
-      if (err) { console.error(err); }
-      reply(event);
+      if (err) {
+        console.error(err);
+      } else {
+        reply({event: event, user: request.auth.credentials});
+      }
     });
   }
 };
--- www/js/controllers/event.js
+++ www/js/controllers/event.js
@@ -1,19 +1,31 @@
 var myApp = angular.module('ecco');
 
-myApp.controller('event', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http) {
+myApp.controller('event', ['$scope', '$stateParams', '$http', '$location', function($scope, $stateParams, $http, $location) {
   $http.get('/event/'+$stateParams.id).success(function(data, status, headers, config) {
-    $scope.event = data;
+    $scope.event = data.event;
+    $scope.user = data.user;
+    $scope.attending = searchEventForUser($scope.event, $scope.user._id);
   }).error(function(data, status, headers, config) {
     console.log("ERROR");
   });
 
   $scope.attend = function() {
-    console.log($scope.event._id);
     $http.post('/event/' + $scope.event._id).success(function(data, status, headers, config) {
-      console.log('Succeded', status);
-      window.location = '/';
+      // window.location = '/';
+      $scope.user = data.user;
+      $scope.event = data.event;
+      $scope.attending = searchEventForUser($scope.event, $scope.user._id);
     }).error(function(data, status, headers, config) {
-      console.log('Erro', status);
+      console.log('Error', status);
     });
   }
 }]);
+
+function searchEventForUser(event, userId) {
+  var result= false;
+  event.attendees.forEach(function(person) {
+    if (person._id === userId)
+      result = true;
+  });
+  return result;
+}
