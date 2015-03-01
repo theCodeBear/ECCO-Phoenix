b149d6ab0d773dea0f36c3f5241b4b543ee85f7a
Jose Zamudio <zamudio@outlook.com>
2015-03-01 02:55:44 -0800

    Add favicon

--- server/routes/event/show.js
+++ server/routes/event/show.js
@@ -4,7 +4,6 @@ var Event = require('../../models/event');
 
 module.exports = {
   handler: function(request,reply) {
-    // console.log("show request: ", request.params);
     Event.findById(request.params.eventId, function(err, event) {
       if (err) { console.error(err); }
       reply(event);
--- www/js/controllers/event.js
+++ www/js/controllers/event.js
@@ -3,7 +3,6 @@ var myApp = angular.module('ecco');
 myApp.controller('event', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http) {
   console.log($stateParams.id);
   $http.get('/event/'+$stateParams.id).success(function(data, status, headers, config) {
-    // console.log(data);
     $scope.event = data;
   }).error(function(data, status, headers, config) {
     console.log("ERROR");
