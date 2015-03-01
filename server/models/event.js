00c11be510a52c0dd63ddeb2879c36d04836a528
Jose Zamudio <zamudio@outlook.com>
2015-02-28 18:26:23 -0800

    Create event form

--- server/models/event.js
+++ server/models/event.js
@@ -4,10 +4,12 @@ var eventSchema = mongoose.Schema({
   createdAt: {type: Date, default: Date.now, required: true},
   name: {type: String, required: true},
   picture: {type: String, required: true},
+  backgroundPicture: {type: String},
   description: {type: String, required: true},
   location: {type: String, required: true},
   geoLocation: {type: String, required: true},
-  eventDate: {type: Date, required: true},
+  startDate: {type: Date, required: true},
+  endDate: {type: Date, required: true},
   twitterHandler: {type: String},
   twitterHash: {type: String},
   attendees: [{type: mongoose.Schema.ObjectId, ref: 'User'}]
